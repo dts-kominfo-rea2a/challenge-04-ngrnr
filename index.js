294926c435e035e1caca380a36f775b1db599d36
ngr.r <91729773+ngrnr@users.noreply.github.com>
2022-06-03 07:59:15 +0700

    Nugraha Ramadan - commited

--- index.js
+++ index.js
@@ -8,12 +8,28 @@ const dates = [
 ];
 
 // TODO: Buatlah fungsi createDate
-const createDate = null;
+const createDate = (dates, id = -1) => {
+  if (id == -1){
+      result = [];
+      for(idDates = 0; idDates < dates.length; idDates++){
+          result.push(Date.parse(dates[idDates])/1000);
+      }
+      return sortDates(result).join('-');
+  }
+
+  else {
+      return (Date.parse(dates[id])/1000).toString();
+  }
+};
+
+const sortDates = (dates) => {
+  return dates.sort()
+}
 
 // ! JANGAN DIMODIFIKASI
 (() => {
   // IIFE
-
+  
   // '1546387200-1580662800-1614841200-1617573600-1651802400' (dalam string)
   console.log(createDate?.(dates));
 
@@ -25,3 +41,6 @@ module.exports = {
   dates,
   createDate,
 };
+
+
+
